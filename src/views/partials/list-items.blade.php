c29acbdb02e323a563ea0b3f5358b0604b253f38
Codiiv <janviersoft@gmail.com>
2018-12-07 07:23:25 -0500

    Added list items partial
    Added list items partial

---
+++ src/views/partials/list-items.blade.php
@@ -0,0 +1,21 @@
+<ul class="the-items">
+  <form class="delete-taxonomy-term" action="{{ url( \Config::get('taxonomies.taxonomy_path').'/delete/taxonomy' ) }}" method="post">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}"> <?php //<meta name="csrf-token" content="{{ csrf_token() }}"> ?>
+    <input type="hidden" name="tobedeleted" value=""> <?php //<meta name="csrf-token" content="{{ csrf_token() }}"> ?>
+    <input type="hidden" name="taxonomy" value="{{ $taxonomy }}"> <?php //<meta name="csrf-token" content="{{ csrf_token() }}"> ?>
+    <?php if(isset($_GET['page'])){ ?>
+      <input type="hidden" name="page" value="{{ $_GET['page'] }}">
+    <?php } ?>
+  </form>
+  @foreach($paginatedTerms as $key => $term )
+  <li data-value="{{ $term->id }}" class="level-{{ $term->level }} @if((isset($term_exists) && $term_exists) && $the_term->id == $term->id) beingedited @endif">
+    <a href="{{ url(Config::get('taxonomies.taxonomy_path')).'?taxonomy='.$taxonomy.'&term_id='.$term->id }}<?php if(isset($_GET['page'])) echo '&page='.$_GET['page']; ?>"><span class="tax-color" style="background-color:{{ $term->color }}"></span> {{ $term->pointer.' '.$term->name }} <span class="theslug">[ {{ $term->slug }} ]</span></a>
+    <div class="taxonomies-actions" style="display:none">
+      <button type="button" name="button" class="btn-button disable-taxonomy btn-normal" disabled>{{ __("Disable") }}</button><button type="button" name="button" class="btn-button btn-dangerous delete-taxonomy" data-termid="{{ $term->id }}">{{ __("Delete") }}</button>
+    </div>
+  </li>
+  @endforeach
+</ul>
+<div class="pagination-container">
+  {{ $paginatedTerms->links() }}
+</div>
