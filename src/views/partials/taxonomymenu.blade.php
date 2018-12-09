04791d7f67597d266c8c6a36951ba8ef04512e46
Codiiv <janviersoft@gmail.com>
2018-12-09 09:16:12 -0500

    Add taxonomy functionality
    Add taxonomy functionality

--- src/views/partials/taxonomymenu.blade.php
+++ src/views/partials/taxonomymenu.blade.php
@@ -1,7 +1,10 @@
-<div class="taxonomies-menu">
+<div class="taxonomies-menu" style="clear:both">
   <ul>
     @foreach($taxonomies as $key => $tax)
-      <li><a href="{{ '?taxonomy='.$key }}">{{ $tax['labels']['name'] }}</a></li>
+      <li class="taxonomy-item {{ ($key == $taxonomy) ? 'active':'' }}"><a href="{{ '?taxonomy='.$key }}">{{ $tax['labels']['name'] }}</a></li>
     @endforeach
+    @if(isset($_GET['taxonomy']))
+      <li class="add-tax"> <a href="{{ url()->current() }}?taxonomy={{ $_GET['taxonomy'] }}">{{ __("Add new").' '.$taxonomies[$_GET['taxonomy']]['labels']['singular_name'] }}</a> </li>
+    @endif
   </ul>
 </div>
