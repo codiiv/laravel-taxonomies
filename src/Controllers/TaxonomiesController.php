18e7f0487f8c66cab424494f73e9ecc7cf07b395
Codiiv <janviersoft@gmail.com>
2019-02-03 14:54:33 -0500

    CHanged models

--- src/Controllers/TaxonomiesController.php
+++ src/Controllers/TaxonomiesController.php
@@ -16,43 +16,6 @@ class TaxonomiesController extends Controller
   {
       // $this->middleware('auth');
   }
-  /*
-  "taxonomies" => [
-    "category"=>[
-      'labels' => [
-        [
-          'name'                       => __( 'Categories' ),
-          'singular_name'              => __( 'Category' ),
-          'menu_name'                  => __( 'Taxonomy' ),
-          'all_items'                  => __( 'All Items' ),
-          'parent_item'                => __( 'Parent Item' ),
-          'parent_item_colon'          => __( 'Parent Item:' ),
-          'new_item_name'              => __( 'New Item Name' ),
-          'add_new_item'               => __( 'Add New Item' ),
-          'edit_item'                  => __( 'Edit Item' ),
-          'update_item'                => __( 'Update Item' ),
-          'view_item'                  => __( 'View Item' ),
-          'separate_items_with_commas' => __( 'Separate items with commas' ),
-          'add_or_remove_items'        => __( 'Add or remove items' ),
-          'choose_from_most_used'      => __( 'Choose from the most used' ),
-          'popular_items'              => __( 'Popular Items' ),
-          'search_items'               => __( 'Search Items' ),
-          'not_found'                  => __( 'Not Found' ),
-          'no_terms'                   => __( 'No items' ),
-          'items_list'                 => __( 'Items list' ),
-          'items_list_navigation'      => __( 'Items list navigation' ),
-        ]
-      ],
-      'hierarchical'               => true,
-      'public'                     => true,
-      'show_ui'                    => true,
-      'show_admin_column'          => true,
-      'show_in_nav_menus'          => true,
-      'show_tagcloud'              => true,
-    ],
-
-  ]
-   */
 
   public function loadIndex(Request $request){
 
--- src/Models/Taxonomies.php
+++ src/Models/Taxonomies.php
@@ -32,13 +32,13 @@ class Taxonomies extends Model
       }
       if($unique_to!=""){
         if($level==0){
-          $terms = Taxonomies::whereNull('parent_id')->where('unique_to', $unique_to)->where("taxonomy", $taxonomy)->with('children')->paginate(\Config::get('taxonomies.terms_per_page'));
+          $terms = Taxonomies::whereNull('parent_id')->where('unique_to', $unique_to)->where("taxonomy", $taxonomy)->with('children')->get();
         }else{
-          $terms = Taxonomies::where("parent_id", $parentID)->where('unique_to', $unique_to)->where("taxonomy", $taxonomy)->with('children')->paginate(\Config::get('taxonomies.terms_per_page'));
+          $terms = Taxonomies::where("parent_id", $parentID)->where('unique_to', $unique_to)->where("taxonomy", $taxonomy)->with('children')->get();
         }
       }else{
         if($level==0){
-          $terms = Taxonomies::whereNull('parent_id')->where("taxonomy", $taxonomy)->with('children')->paginate(\Config::get('taxonomies.terms_per_page'));
+          $terms = Taxonomies::whereNull('parent_id')->where("taxonomy", $taxonomy)->with('children')->get();
         }else{
           $terms = Taxonomies::where("parent_id", $parentID)->where("taxonomy", $taxonomy)->with('children')->get();
         }
